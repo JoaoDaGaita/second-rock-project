4884d8369ff2d8e4b2c3461e277c0cf2742ff02d
João <joaovsilva70@outlook.com>
2023-10-20 18:48:34 -0300

    adc

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import { Play } from "phosphor-react"
+import { HandPalm, Play } from "phosphor-react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import zod from "zod"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { differenceInSeconds } from "date-fns"
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
@@ -10,6 +11,7 @@ const newCycleFormValidationSchema = zod.object({
     .number()
     .min(5, "O ciclo precisa ser de no máximo 5 min.")
     .max(60, "O ciclo precisa ser de no mínimo 60 min."),
+
 })
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
@@ -18,10 +20,14 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
 }
 
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -31,18 +37,57 @@ export function Home() {
     },
   })
 
-  function createNewCycle(data: NewCycleFormData) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
     const newCycle: Cycle = {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date()
     }
 
     setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
+
+  function handleInterruptCycle() {
+
+  }
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    let interval: number
+
+    if(activeCycle) {
+      interval = setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        )
+      })
+    }
+
+    return() => {
+      clearInterval(interval)
+    }
+  }, [activeCycle])
+
+  useEffect(() => {
+    if(activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  })  
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60  
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
 
   const task = watch("task")
   const isSubmitDisabled = !task
@@ -50,7 +95,7 @@ export function Home() {
   return (
     <div className="flex-1 flex flex-col items-center mt-[72px] justify-center ">
       <form
-        onSubmit={handleSubmit(createNewCycle)}
+        onSubmit={handleSubmit(handleCreateNewCycle)}
         className="flex flex-col items-center gap-14"
       >
         <div className="w-full flex items-center justify-center gap-2 text-gray-100 text-lg font-bold flex-wrap">
@@ -58,6 +103,7 @@ export function Home() {
           <input
             id="task"
             {...register("task")}
+            disabled={!!activeCycle}
             className="bg-transparent h-10 border-0 border-b-2 border-solid border-gray-500 
              font-bold text-lg py-0 px-2 text-gray-100 flex-1 placeholder:text-gray-400 focus:shadow-none
              focus:border-green-500 focus:outline-none"
@@ -72,6 +118,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle}
             {...register("minutesAmount", { valueAsNumber: true })}
             className="
           bg-transparent w-16 h-10 border-0 border-b-2 border-solid border-gray-500 
@@ -86,15 +133,26 @@ export function Home() {
           className="font-robotoMono text-[10rem] leading-[8rem] text-gray-100 flex gap-4
          [&>span]:bg-gray-700 [&>span]:py-8 [&>span]:px-4 [&>span]:rounded-lg"
         >
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <span className="py-8 px-0 text-green-500 w-16 flex overflow-hidden justify-center">
             :
           </span>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </div>
 
+        { activeCycle ? (
+          <button
+          type="button"          
+          className="flex w-full py-4 px-10 justify-center items-center gap-2
+           shrink-0 rounded-lg bg-red-500 cursor-pointer
+            disabled:opacity-70 disabled:cursor-not-allowed [&:not(:disabled):hover]:bg-red-700"
+        >
+          <HandPalm size={24} />
+          Interromper
+        </button>
+        ) : (
         <button
           type="submit"
           disabled={isSubmitDisabled}
@@ -105,6 +163,7 @@ export function Home() {
           <Play size={24} />
           Começar
         </button>
+        ) }
       </form>
     </div>
   )
